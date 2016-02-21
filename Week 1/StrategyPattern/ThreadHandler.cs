58f97ef2c6a609b90b195c77642f5367a0c64e12
Daria <padfone66@gmail.com>
2016-02-21 18:04:16 +0100

    Start

---
+++ Week 1/StrategyPattern/ThreadHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyPattern
+{
+   public class ThreadHandler
+    {
+        ProcessingBehaviour pb;
+        public void PerformRun()
+        {
+            pb.Run();
+        }
+
+        Random rnd = new Random();
+        public int myNumber;
+        myNumber = rnd.Next(1, 100);
+        public List<int> randomList = new List<int>();
+        
+     
+    }
+}
