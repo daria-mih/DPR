0f84c831177780f347f287d45737e629186a7694
Tycho Hertogs <tycho94@hotmail.com>
2016-03-06 15:33:08 +0100

    Oops

---
+++ Week 3/FactoryPattern/BrandFactory.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    class SamsungFactory : AbstractFactory
+    {
+        public IPhablet createPhablet()
+        {
+            
+        }
+
+        public IPhone createPhone()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ITablet createTablet()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class AppleFactory : AbstractFactory
+    {
+        public IPhablet createPhablet()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IPhone createPhone()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ITablet createTablet()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class NokiaFactory : AbstractFactory
+    {
+        public IPhablet createPhablet()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IPhone createPhone()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ITablet createTablet()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Week 3/FactoryPattern/BrandProducts.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    //Samsung products
+    class GalaxyNote : IPhablet
+    {
+        public void Call()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PlayVideo()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class GalaxyMini:IPhone
+    {
+    }
+    class GalaxyTab:ITablet
+    {
+    }
+}
