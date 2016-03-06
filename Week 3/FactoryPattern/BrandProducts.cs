f513367d095ea90cd3fad8814e28d8b8fa4aa589
Daria <padfone66@gmail.com>
2016-03-06 15:41:22 +0100

    .

--- Week 3/FactoryPattern/BrandProducts.cs
+++ Week 3/FactoryPattern/BrandProducts.cs
@@ -19,10 +19,99 @@ namespace FactoryPattern
             throw new NotImplementedException();
         }
     }
-    class GalaxyMini:IPhone
+    class GalaxyMini : IPhone
     {
+        public void Call()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Message()
+        {
+            throw new NotImplementedException();
+        }
     }
-    class GalaxyTab:ITablet
+    class GalaxyTab : ITablet
     {
+        public void PlayVideoHD()
+        {
+            throw new NotImplementedException();
+        }
     }
+
+    //AppleProducts
+
+    class iPhone : IPhone
+    {
+        public void Call()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Message()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class iPad : ITablet
+    {
+        public void PlayVideoHD()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class iPhonePlus : IPhablet
+    {
+        public void Call()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PlayVideo()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    //Nokia
+
+    class Lumia : IPhone
+    {
+        public void Call()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Message()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class Lumia1520 : IPhablet
+    {
+        public void Call()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PlayVideo()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class NokiaN1 : ITablet
+    {
+        public void PlayVideoHD()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+
 }
+
