84b23e41739357fbca3715051d2ef5524ab78e4f
Daria <padfone66@gmail.com>
2016-03-27 22:34:53 +0200

    started a project

---
+++ Week 6/VisitorPattern/VisitorPattern/Shipping.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisitorPattern
+{
+    public class Shipping : IVisitor
+    {
+
+        public double visit(Laptops laptops)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Week 6/VisitorPattern/VisitorPattern/ShippingPromotion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisitorPattern
+{
+    public class ShippingPromotion : IVisitor
+    {
+        public double visit(Laptops laptops)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
